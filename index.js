8031f5a28c59d4405df148141325924d31a860f5
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-03 19:32:04 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names,event){
+
+    let newNames=[]
+
+    for(let counter=0;counter<names.length;counter++){
+
+    let x=`Thank you, ${names[counter]}, for the wonderful ${event} gift!`
+
+    newNames.push(x)
+    }
+return newNames;
+}
+
+function countDown(num){
+ while(num>=0){
+    console.log(num)
+    num--;
+ }
+
+
+}
